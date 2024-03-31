f042dbb724ed860d4c6f10a5b566447c3fe17405
lefasom <leosombra89@gmail.com>
2024-03-31 09:49:55 -0300

    pruebando pull

--- angular/src/app/user/user.component.ts
+++ angular/src/app/user/user.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  username = "Lefasom"
+  username = "Leo Sombra"
   session = false
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -33,3 +33,4 @@ async function bootstrap() {
 
 }
 bootstrap();
+// archivo raiz
